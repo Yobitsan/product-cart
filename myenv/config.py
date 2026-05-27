9fbccc680e4edd109d0bbfa648b2bc22cbcdc7a2
Jasim <jasimhamdulay@gmail.com>
2026-05-27 11:34:29 +0300

    db change

--- myenv/config.py
+++ myenv/config.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
-db_url = "postgresql://postgres:ballsack2021@localhost:5432/fastapi1"
+import os
+#db_url = "postgresql://postgres:ballsack2021@localhost:5432/fastapi1"
+db_url=os.getenv("DATABASE_URL")
 engine = create_engine(db_url)
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
